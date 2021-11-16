4c48736bd9f42fc88a708b76cd4558c9bbde6295
Trevor Cickovski <tcickovs@delphi.cs.fiu.edu>
2021-11-16 16:00:37 -0500

    Fixed to write file

--- AssemblyStatsPlugin.cpp
+++ AssemblyStatsPlugin.cpp
@@ -14,7 +14,7 @@ void AssemblyStatsPlugin::run() {
 void AssemblyStatsPlugin::output(std::string file) {
  // spades.py -o 02_AssemblyStats -1 $1 -2 $2 --careful
  // assembly-stats -t ./All_assemblies/*.fasta > ./All_assemblies/00_Metrics_all_assemblies.txt
-	std::string command = "assembly-stats -t "+inputfile+"/*.fasta";
+	std::string command = "assembly-stats -t "+inputfile+"/*.fasta > "+file;
  std::cout << command << std::endl;
 
  system(command.c_str());
